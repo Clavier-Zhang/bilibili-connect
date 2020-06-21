8a7e73b5d335a1e70e4020aee0783923a6286888
Clavier-Zhang <y2595zha@edu.uwaterloo.ca>
2020-06-20 23:58:58 -0400

    Implement block category, dark mode

---
+++ src/App.js
@@ -0,0 +1,118 @@
+/* global chrome */
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import CategorySwitch from './components/CategorySwitch'
+import { Grid } from '@material-ui/core';
+
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <Grid container alignItems="center" xs={10}>
+
+                        <Grid item xs={12}>
+                            <img src={logo} className="App-logo" alt="logo" />
+                        </Grid>
+
+                        <Grid item xs={4}>
+                            <CategorySwitch title="直播" selector="#bili_live"/>
+                        </Grid>
+
+                        <Grid item xs={4}>
+                            <CategorySwitch title="推广" selector="#reportFirst2"/>
+                        </Grid>
+
+                        <Grid item xs={4}>
+                            <CategorySwitch title="舞蹈" selector="#bili_dance"/>
+                        </Grid>
+
+                        <Grid item xs={4}>
+                            <CategorySwitch title="游戏" selector="#bili_game"/>
+                        </Grid>
+
+                        <Grid item xs={4}>
+                            <CategorySwitch title="数码" selector="#bili_digital"/>
+                        </Grid>
+
+                        <Grid item xs={4}>
+                            <CategorySwitch title="专栏" selector="#bili_read"/>
+                        </Grid>
+
+                        <Grid item xs={4}>
+                            <CategorySwitch title="时尚" selector="#bili_fashion"/>
+                        </Grid>
+
+                        <Grid item xs={4}>
+                            <CategorySwitch title="知识" selector="#bili_technology"/>
+                        </Grid>
+
+                        <Grid item xs={4}>
+                            <CategorySwitch title="生活" selector="#bili_life"/>
+                        </Grid>
+
+                        <Grid item xs={4}>
+                            <CategorySwitch title="鬼畜" selector="#bili_kichiku"/>
+                        </Grid>
+
+                        <Grid item xs={4}>
+                            <CategorySwitch title="娱乐" selector="#bili_ent"/>
+                        </Grid>
+
+                        <Grid item xs={4}>
+                            <CategorySwitch title="资讯" selector="#bili_information"/>
+                        </Grid>
+
+                        <Grid item xs={4}>
+                            <CategorySwitch title="影视" selector="#bili_cinephile"/>
+                        </Grid>
+
+                        <Grid item xs={4}>
+                            <CategorySwitch title="国创" selector="#bili_guochuang"/>
+                        </Grid>
+
+                        <Grid item xs={4}>
+                            <CategorySwitch title="漫画" selector="#bili_manga"/>
+                        </Grid>
+
+                        <Grid item xs={4}>
+                            <CategorySwitch title="音乐" selector="#bili_music"/>
+                        </Grid>
+
+                        <Grid item xs={4}>
+                            <CategorySwitch title="电影" selector="#bili_movie"/>
+                        </Grid>
+
+                        <Grid item xs={4}>
+                            <CategorySwitch title="电视剧" selector="#bili_teleplay"/>
+                        </Grid>
+
+                        <Grid item xs={5}>
+                            <CategorySwitch title="记录片" selector="#bili_documentary"/>
+                        </Grid>
+
+                        <Grid item xs={6}>
+                            <CategorySwitch title="特别推荐" selector="#bili_report_spe_rec"/>
+                        </Grid>
+
+                        <Grid item xs={4}>
+                            <CategorySwitch title="深色模式" selector="isDarkMode"/>
+                        </Grid>
+                        
+                    </Grid>
+                </header>
+            </div>
+        )
+    }
+    
+  
+}
+
+export default App;
---
+++ src/components/CategorySwitch.js
@@ -0,0 +1,47 @@
+/* global chrome */
+import React from 'react';
+import { Switch, Grid } from '@material-ui/core';
+
+
+class CategorySwitch extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: false
+        }
+    }
+
+    componentWillMount() {
+        chrome.storage.sync.get(this.props.selector, (items) => {
+            console.log('*****')
+            console.log(items[this.props.selector], this.props.selector)
+            this.setState({
+                value: items[this.props.selector] === undefined ? true : items[this.props.selector]
+            })
+        })
+    }
+
+    switchOnChange(event) {
+        this.setState({
+            value: event.target.checked
+        })
+        chrome.storage.sync.set({
+          [this.props.selector]: event.target.checked
+        })
+    }
+
+    render() {
+        return (
+            <Grid component="label" container alignItems="center" spacing={1}>
+                <Grid item>{this.props.title}</Grid>
+                <Grid item>
+                    <Switch onChange={this.switchOnChange.bind(this)} checked={this.state.value} label="Small" />
+                </Grid>
+            </Grid>
+        )
+    }
+    
+}
+
+export default CategorySwitch;
